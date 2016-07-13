0b6558c6303b41017284fffc538eeb9aa11105e1
Diana Duong <dduong013@gmail.com>
2016-07-13 16:19:02 -0500

    added factory services for calling the json file with countries object

--- app/cc-app.js
+++ app/cc-app.js
@@ -1,6 +1,7 @@
 angular
 	.module('cc-App', ['ngRoute', 'ngMessages', 'ngAnimate'])
-	.config(['$routeProvider', function($routeProvider){
+	.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider){
+		$httpProvider.defaults.useXDomain = true;
 		$routeProvider.when('/', {
 			templateUrl : 'home.html',
 			controller : 'HomeCtrl as vm'
@@ -14,12 +15,34 @@ angular
 			controller : 'CountryDetailCtrl as vm'
 		})
 	}])
+
+
+	.factory('countriesList', ['$http', '$q', 'cc_endpoint', 'cc_countries_json', function($http, $q, cc_endpoint, cc_countries_json){
+		return function(params){
+
+		var reqParams = {
+			country : 'default',
+			lang : 'default',
+			type: 'JSON'
+		};
+		return $http.get(cc_countries_json, {params: reqParams})
+			.then(function(response){
+				return $q.when(response.data);
+			})
+		};
+	}])
 	.controller('HomeCtrl', function(){
 
 	})
-	.controller('CountryListCtrl', function(){ 
-
-	})
-	.controller('CountryDetailCtrl', function(){ 
-
-	})
+	.controller('CountryListCtrl', ['$scope', 'countriesList', function($scope, countriesList){ 	
+		countriesList()
+			.then(function(response) {
+		    	$scope.data = response.data;
+		})
+	}])
+	.controller('CountryDetailCtrl',['$scope', 'countriesList',function($scope, countriesList){ 
+		countriesList()
+			.then(function(response) {
+		    	$scope.data = response.data;
+		})
+	}])
